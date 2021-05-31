8b650a17a3f9ef9959f98f7096a10ea118b14135
Mark Nagibin <myrkwill@icloud.com>
2021-05-31 16:48:08 +0300

    Initial Commit

---
+++ Shared/BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserApp.swift
+//  Shared
+//
+//  Created by Mark Nagibin on 31.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
